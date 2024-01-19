70f2fde2b60d2fb4ea27776f9be2a8bd312ae33d
eduluisf <eduluisfeduardo@gmail.com>
2024-01-19 17:02:23 -0500

    FinalCommit_Eduardo
    this is the final Commit

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -3,10 +3,8 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    // Nombre de la escena a la que quieres cambiar
-  
 
-    // Método para cambiar de escena
+  
     public void ChangeToScene()
     {
         SceneManager.LoadScene("Game");
